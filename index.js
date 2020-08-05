ab8182eb7819f6ef5d7f378c962aab20623e1a40
BlairCalgary <59458311+BlairCalgary@users.noreply.github.com>
2020-08-05 15:55:16 -0600

    added animation

--- index.js
+++ index.js
@@ -10,3 +10,71 @@ function scrollFunction() {
     // document.getElementById("logo").style.fontSize = "25px";
   }
 }
+
+var scroll = window.requestAnimationFrame ||
+  function(callback) {window.setTimeout(callback,1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function isElementInViewport (el) 
+{
+    //special bonus for those using jQuery
+    if (typeof jQuery !== 'undefined' && el instanceof jQuery) el = el[0];
+
+    var rect = el.getBoundingClientRect();
+    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    return (
+           (rect.left >= 0)
+        && (rect.top >= 0)
+        && ((rect.left + rect.width) <= windowWidth)
+        && ((rect.top + rect.height) <= windowHeight)
+    );
+
+}
+
+function isElementPartiallyInViewport(el)
+{
+    //special bonus for those using jQuery
+    if (typeof jQuery !== 'undefined' && el instanceof jQuery) el = el[0];
+
+    var rect = el.getBoundingClientRect();
+    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    var windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    var windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    var vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    var horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+    return (vertInView && horInView);
+}
+
+// function isElementInViewPort(el) {
+//   if (typeof jQuery === "function" && el instanceof jQuery) {
+//     el = el[0];
+//   } var rect = el.getBoundingClientRect();
+//   return (
+//     (rect.top <= 0 && rect.bottom >= 0)
+//     ||
+//     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+//     ||
+//     (rect.top >= 0 &&
+//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+//     );
+// }
+
+function loop() {
+  elementsToShow.forEach(function (element) {
+    if (isElementPartiallyInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+  scroll(loop);
+}
+
+loop();
+
